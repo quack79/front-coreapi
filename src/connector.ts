52c84e36de9eae481d00dbacda27f263d52f906e
quack79 <alex@quack.me>
2024-04-25 14:30:28 +0100

    .

--- src/connector.ts
+++ src/connector.ts
@@ -1,7 +1,7 @@
 import needle, { NeedleResponse } from 'needle'
 import 'dotenv/config'
 
-const { log2f, log2c } = require("./logging");
+const log2c = require("./logging");
 
 export class FrontConnector {
     static readonly headers = {
@@ -40,7 +40,7 @@ export class FrontConnector {
 
     private static async makeRateLimitedRequest(method: string, url: string): Promise<NeedleResponse> {
         const options = { headers: this.headers };
-        log2c.warn(`Request: ${url}`);
+        log2c.info(`Request: ${url}`);
         let response: NeedleResponse;
         do {
             response = await needle('get', url, null, options);
--- src/logging.ts
+++ src/logging.ts
@@ -1,6 +1,16 @@
 // Winston Logging
 const winston = require("winston");
 
+const log2c = winston.createLogger({
+    level: "info",
+    format: winston.format.cli(),
+    transports: [new winston.transports.Console()],
+});
+
+module.exports = log2c;
+
+
+/*
 const log2f = winston.createLogger({
     transports: [
         new winston.transports.File({
@@ -9,12 +19,5 @@ const log2f = winston.createLogger({
         })
     ]
 });
-
-const log2c = winston.createLogger({
-    level: process.env.LOG_LEVEL || 'info',
-    format: winston.format.cli(),
-    transports: [new winston.transports.Console()],
-});
-
-module.exports = log2f, log2c;
+*/
 // Winston Logging
